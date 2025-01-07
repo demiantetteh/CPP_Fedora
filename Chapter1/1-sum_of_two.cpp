39f3b0c0957f62f13b099e8aeb2ab7e6ccf9a291
demiantetteh <demiantetteh@gmail.com>
2025-01-07 14:39:41 -0700

    sum of two numbers

--- Chapter1/1-sum_of_two.cpp
+++ Chapter1/1-sum_of_two.cpp
@@ -2,7 +2,24 @@
 
 int main()
 {
-    std::cout << "Happy Birthday to you" << std::endl;
+    int first, second;
+    int sum, prod;
+
+    std::cout << "Enter your first favourite number and hit the the return key"
+              << std::endl;
+    std::cin >> first;
+
+    std::cout << "Enter your second favourite number and hit the return key"
+              << std::endl;
+    std::cin >> second;
+
+    sum = first + second;
+    prod = first * second;
+
+    std::cout << "The sum is " << sum << std::endl;
+    std::cout << "Theh product is " << prod << std::endl;
+
+    std::cout << "\nThis is the end of the program" << std::endl;
 
     return (0);
 }
